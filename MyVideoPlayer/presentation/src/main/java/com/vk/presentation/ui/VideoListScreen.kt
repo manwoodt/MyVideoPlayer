c73ef5b90d7b58f94a938b92612960463169d373
manwoodt <121170372+manwoodt@users.noreply.github.com>
2025-02-21 13:53:02 +0300

    add topBar

--- MyVideoPlayer/presentation/src/main/java/com/vk/presentation/ui/VideoListScreen.kt
+++ MyVideoPlayer/presentation/src/main/java/com/vk/presentation/ui/VideoListScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -34,7 +35,7 @@ fun VideoListScreen(onVideoSelected: (VideoInfo) -> Unit = {}) {
     }
 
 
-    Scaffold() { innerPadding ->
+    Scaffold(topBar = { TopAppBar(title = {Text("Видео")}) }) { innerPadding ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
