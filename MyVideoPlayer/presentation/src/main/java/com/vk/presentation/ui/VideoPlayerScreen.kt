c4ff3150d0566e72da069be05627362a5598e738
manwoodt <121170372+manwoodt@users.noreply.github.com>
2025-02-21 14:13:19 +0300

    add topBar and arrow back for VideoPlayerScreen

--- MyVideoPlayer/presentation/src/main/java/com/vk/presentation/ui/VideoPlayerScreen.kt
+++ MyVideoPlayer/presentation/src/main/java/com/vk/presentation/ui/VideoPlayerScreen.kt
@@ -3,7 +3,19 @@ package com.vk.presentation.ui
 import android.net.Uri
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
@@ -18,9 +30,34 @@ import androidx.media3.ui.PlayerView
 @Composable
 //добавить кнопку назад
 fun VideoPlayerScreen(video: VideoInfo, onBack: () -> Unit) {
+
+    Scaffold(topBar = { videoPlayerTopBar(onBack) },
+        content = { innerPadding ->
+            VideoPlayerContent(video, innerPadding)
+        })
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun videoPlayerTopBar(onBack: () -> Unit) {
+    TopAppBar(
+        title = { Text("Воспроизведение видео") },
+        navigationIcon = {
+            IconButton(onClick = { onBack() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun VideoPlayerContent(video: VideoInfo, innerPadding: PaddingValues) {
     val context = LocalContext.current
-    val exoPlayer = remember{
-        ExoPlayer.Builder(context).build().apply{
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build().apply {
             setMediaItem(MediaItem.fromUri(Uri.parse(video.srcUrl)))
             prepare()
             playWhenReady = true
@@ -32,18 +69,24 @@ fun VideoPlayerScreen(video: VideoInfo, onBack: () -> Unit) {
             exoPlayer.release()
         }
     }
-
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = { ctx ->
-            PlayerView(ctx).apply {
-                player = exoPlayer
-                layoutParams = FrameLayout.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+    ) {
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { ctx ->
+                PlayerView(ctx).apply {
+                    player = exoPlayer
+                    layoutParams = FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                }
             }
-        }
-    )
-
+        )
+    }
 }
+
+
